54be3231914ce7bc3991d625dddbb48a581f9018
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-09-24 21:30:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let colorSelect= document.getElementById("colorSelect");
+let inputVal=document.getElementById("input");
+
+inputVal.addEventListener("click", (e)=>{
+	colorSelect.remove(e.target.value)
+})
